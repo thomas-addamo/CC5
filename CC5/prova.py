b044fc4f0b0be4019eb922df2d72c03b64fab429
DanyMazze <daniele.mazzetti08@gmail.com>
2025-12-09 08:21:32 +0100

    agginto in prova.py il pezzo di codice che legge il json

--- CC5/prova.py
+++ CC5/prova.py
@@ -6,34 +6,10 @@ from potion import Potion
 import random as r
 
 def create_weapon(player: Player) -> None:
-    melee = [
-        Weapon("Sword", r.randint(8,12), r.randint(13,18), "melee"),
-        Weapon("Axe", r.randint(10,15), r.randint(16,20), "melee"),
-        Weapon("Dagger", r.randint(5,10), r.randint(11,15), "melee"),
-        Weapon("Mazzetta", 1, r.randint(2,100), "melee"),
-        Weapon("long sword", r.randint(12,18), r.randint(19,25), "melee"),
-        Weapon("short sword", r.randint(7,11), r.randint(12,16), "melee"),
-        Weapon("Katana", r.randint(10,14), r.randint(15,22), "melee"),
-        Weapon("Claymore", r.randint(15,20), r.randint(20,30), "melee"),
-        Weapon("Spada Laser", r.randint(20,30), r.randint(30,50), "melee"),
-        Weapon("Mace", r.randint(9,13), r.randint(14,19), "melee"),
-        Weapon("Hammer", r.randint(11,16), r.randint(17,22), "melee"),
-        Weapon("raipier", r.randint(6,10), r.randint(11,15), "melee")
-    ]
-    ranged = [
-        Weapon("Bow", r.randint(5,10), r.randint(11,15), "ranged"),
-        Weapon("Crossbow", r.randint(7,12), r.randint(13,17), "ranged"),
-        Weapon("Throwing Knives", r.randint(4,9), r.randint(10,14), "ranged"),
-        Weapon("Slingshot", r.randint(1,5), r.randint(7,12), "ranged"),
-        Weapon("blowgun", r.randint(3,8), r.randint(9,13), "ranged"),
-        Weapon("Sniper Rifle", r.randint(15,25), r.randint(26,35), "ranged"),
-        Weapon("Pistol", r.randint(10,15), r.randint(16,20), "ranged"),
-        Weapon("Rifle", r.randint(12,18), r.randint(19,25), "ranged"),
-        Weapon("Shotgun", r.randint(14,20), r.randint(21,30), "ranged"),
-        Weapon(" of Mazzetti", r.randint(20,40), r.randint(41,60), "ranged"),
-        Weapon("Laser Gun", r.randint(25,35), r.randint(36,50), "ranged"),
-        Weapon("Plasma Rifle", r.randint(30,45), r.randint(46,70), "ranged")
-    ]
+    with open("weapons.txt", "r") as file:
+        lines = file.readlines()
+    melee = lines[melee]
+    ranged = lines[ranged]
     if player.strength < player.dexterity:
         player.weapon = r.choice(ranged)
     else:
